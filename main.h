bee808b54fdec91f55429299b119ae5105ffb91e
Jessey9 <sundayjesse95@gmail.com>
2023-09-16 13:47:00 -0700

    Main header file for the group projects

--- main.h
+++ main.h
@@ -49,7 +49,7 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
  *			FUNCTIONS			*
  ********************************************************/
 
-/* Showing funtions to print chars and strings */
+/* Showing funtions to print char and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, it precision, int size);
 int print_string(va_list types, char buffer[],
